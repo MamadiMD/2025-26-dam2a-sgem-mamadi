9ce3c9448ad53afad71578975802175546d5b613
MamadiMD <mmagassdam1@educacion.navarra.es>
2025-11-18 09:14:52 +0100

    Ejercicio 2 y 3

---
+++ 2025_11_18_Actividades/Ejercicio2.py
@@ -0,0 +1,3 @@
+import random
+num_aleatorio = random.randint(1,6)
+print("El resultado del dado es:" + str(num_aleatorio))
---
+++ 2025_11_18_Actividades/Ejercicio3.py
@@ -0,0 +1,4 @@
+print("Introduce los minutos a convertir:")
+minutos = int(input())
+segundos = minutos * 60
+print(str(minutos) + " minutos son " + str(segundos) + " segundos")
