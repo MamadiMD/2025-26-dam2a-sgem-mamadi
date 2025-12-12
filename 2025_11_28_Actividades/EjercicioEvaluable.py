e1b1680fc6bb6ec60cc79905998fdc4b06621bc6
MamadiMD <mmagassdam1@educacion.navarra.es>
2025-12-12 09:13:50 +0100

    codigo mejorado

--- 2025_11_28_Actividades/EjercicioEvaluable.py
+++ 2025_11_28_Actividades/EjercicioEvaluable.py
@@ -6,98 +6,87 @@ mario = {"nombre": "Super Mario", "trabajos": [77,82,23,39], "test": [18,60], "p
 
 
 estudiantes = {1:bruce, 2:harry, 3:hermione, 4:peter, 5:mario}
-notas = {"Sobresaliente":[],"Notable":[],"Bien":[],"Suficiente":[],"Necesita mejorar":[]}
+
+
+def calcular_nota_media(estudiante):
+    trabajos = sum(estudiante["trabajos"]) / len(estudiante["trabajos"]) * 0.1
+    tests = sum(estudiante["test"]) / len(estudiante["test"]) * 0.5
+    practicas = sum(estudiante["practicas"]) / len(estudiante["practicas"]) * 0.4
+    notamedia = trabajos + tests + practicas
+    return notamedia
+
+    
+def listarNotas(estudiante):
+    notas = {"Sobresaliente":[],"Notable":[],"Bien":[],"Suficiente":[],"Necesita mejorar":[]}
+    for estudiante in estudiantes.values():
+        nota_media = calcular_nota_media(estudiante)
+        if nota_media >= 90:
+            notas["Sobresaliente"].append(estudiante)
+        elif nota_media >= 70:
+            notas["Notable"].append(estudiante)
+        elif nota_media >= 60:
+            notas["Bien"].append(estudiante)
+        elif nota_media >= 50:
+            notas["Suficiente"].append(estudiante)
+        else:
+            notas["Necesita mejorar"].append(estudiante)
+    return notas
+
+def asignar_calificacion(estudiante):
+    nota_media = calcular_nota_media(estudiante)
+    nombre = estudiante["nombre"]
+    if nota_media >= 90:
+        return (f"Calificacion final de {nombre}: Sobresaliente")
+    elif nota_media >= 70:
+        return (f"Calificacion final de {nombre}: Notable")
+    elif nota_media >= 60:
+        return (f"Calificacion final de {nombre}: : Bien")
+    elif nota_media >= 50:
+        return (f"Calificacion final de {nombre}: Suficiente")
+    else:
+        return (f"Calificacion final de {nombre}: Necesita mejorar")
+
+
+def mostrar_informe(estudiante):
+    nota_media = calcular_nota_media(estudiante)
+    calificacion = asignar_calificacion(estudiante)
+    nombre = estudiante["nombre"]
+    print(nombre)
+    print(f"Nota media final para {nombre} : {round(nota_media,2)}")
+    print(calificacion)
+
+def menu_filtro(estudiantes):
+    seguir = True
+    while(seguir):
+        respuesta = input("Desea filtar por calificacion (S/N): ")
+        if respuesta == "S":
+            notas = listarNotas(estudiantes)
+            filtracion = input("Introduzca uno de los siguientes valores (Sobresaliente - Notable - Bien - Suficiente - Necesita Mejorar - Mostrar todo): ")
+            if filtracion in notas:
+                for estudiante in notas[filtracion]:
+                    mostrar_informe(estudiante)
+            elif filtracion == "Mostrar todo":
+                for estudiante in estudiantes.values():
+                    mostrar_informe(estudiante)
+                    seguir = False
+            else:
+                print("Valor no valido")
+
+        elif respuesta == "N":
+            for estudiante in estudiantes.values():
+                mostrar_informe(estudiante)
+                seguir = False
+        else:
+            print("Introduce S o N")
+
 
 for estudiante in estudiantes.values():
-        notamedia = 0
-        trabajos = 0
-        tests = 0
-        practicas = 0
-        
-        for trabajo in estudiante["trabajos"]:
-            trabajos += trabajo
-        
-        trabajos = (trabajos / len(estudiante["trabajos"])) * 0.1
+        mostrar_informe(estudiante)
 
-        for test in estudiante["test"]:
-            tests += test
+menu_filtro(estudiantes)
 
-        tests = (tests / len(estudiante["test"])) * 0.5
+    
 
-        for practica in estudiante["practicas"]:
-            practicas += practica
-        
-        practicas = (practicas/len(estudiante["practicas"])) * 0.4
-
-        notamedia = trabajos + tests + practicas
-
-        nombre = estudiante["nombre"]
-
-        datos = [nombre,notamedia]
-
-        print(nombre)
-        print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-        print("Nota media final para " + nombre + " : " + str(round(notamedia , 2)))
-        
-        if notamedia >= 90:
-            print("Calificacion final de " + nombre + " : Sobresaliente" )
-            notas["Sobresaliente"].append(datos)
-        elif notamedia >= 70:
-            print("Calificacion final de " + nombre + " : Notable" )
-            notas["Notable"].append(datos)
-        elif notamedia >= 60:
-            print("Calificacion final de " + nombre + " : Bien" )
-            notas["Bien"].append(datos)
-        elif notamedia >= 50:
-            print("Calificacion final de " + nombre + " : Suficiente" )
-            notas["Suficiente"].append(datos)
-        elif notamedia < 50:
-            print("Calificacion final de " + nombre + " : Necesita mejorar" )
-            notas["Necesita mejorar"].append(datos)
-
-
-
-seguir = True
-while(seguir):
-    respuesta = input("Desea filtar por calificacion (S/N): ")
-    if respuesta == "S":
-        filtracion = input("Introduzca uno de los siguientes valores (Sobresaliente - Notable - Bien - Suficiente - Necesita Mejorar)")
-        if filtracion == "Sobresaliente":
-            for sobresaliente in notas["Sobresaliente"]:
-                print(sobresaliente[0])
-                print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-                print("Nota media final para " + sobresaliente[0] + " : " + str(round(sobresaliente[1])))
-                print("Calificacion final de " + sobresaliente[0] + " : Sobresaliente" )
-        elif filtracion == "Notable":
-            for notable in notas["Notable"]:
-                print(notable[0])
-                print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-                print("Nota media final para " + notable[0] + " : " + str(round(notable[1])))
-                print("Calificacion final de " + notable[0] + " : Notable" )
-        elif filtracion == "Bien":
-            for bien in notas["Bien"]:
-                print(bien[0])
-                print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-                print("Nota media final para " + bien[0] + " : " + str(round(bien[1])))
-                print("Calificacion final de " + bien[0] + " : Bien" )
-        elif filtracion == "Suficiente":
-            for suficiente in notas["Suficiente"]:
-                print(suficiente[0])
-                print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-                print("Nota media final para " + suficiente[0] + " : " + str(round(suficiente[1])))
-                print("Calificacion final de " + suficiente[0] + " : Suficiente" )
-        elif filtracion == "Necesita mejorar":
-            for necesita in notas["Necesita mejorar"]:
-                print(necesita[0])
-                print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
-                print("Nota media final para " + necesita[0] + " : " + str(round(necesita[1])))
-                print("Calificacion final de " + necesita[0] + " : Necesita mejorar" )
-        else:
-            print("Valor no valido")
-    elif respuesta == "N":
-        seguir = False
-    else:
-        print("Introduce S o N")
 
         
 
